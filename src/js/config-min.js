cdc8723ea1f5024013b60b9f395d1fe19f66c93f
Gyanthakur <gps.96169@gmail.com>
2024-05-31 16:25:19 +0530

    feat: Added toggle mode

--- src/js/config-min.js
+++ src/js/config-min.js
@@ -1,10 +1,10 @@
 particlesJS("particles-js", {
   particles: {
     number: { value: 80, density: { enable: true, value_area: 800 } },
-    color: { value: "#ffffff" },
+    color: { value: "#000000" },
     shape: {
       type: "circle",
-      stroke: { width: 0, color: "#000000" },
+      stroke: { width: 2, color: "#ffffff" },
       polygon: { nb_sides: 5 },
       image: { src: "img/github.svg", width: 100, height: 100 },
     },
